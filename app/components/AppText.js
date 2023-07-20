7ee56f8316a833edf3d0ec6c513473a9734f0e7b
Nandom Alfred <nandommamdam@gmail.com>
2023-07-20 13:40:24 +0100

    edit some components

---
+++ app/components/AppText.js
@@ -0,0 +1,9 @@
+import {StyleSheet, Text} from 'react-native'
+
+const AppText = ({childred}) => {
+  return <Text>{childred}</Text>
+}
+
+export default AppText
+
+const styles = StyleSheet.create({})
