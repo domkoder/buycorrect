fe7fdb2afb6a235dbe3a11fb2d17c5a8cfe49077
Nandom Alfred <nandommamdam@gmail.com>
2023-07-20 13:41:33 +0100

    made some update to some screens

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,154 +1,154 @@
-import React, { useState } from 'react';
-import {View, StyleSheet, TextInput, Button, Text, TouchableOpacity} from 'react-native';
+import {useState} from 'react'
+import {View, StyleSheet, TextInput, Text, TouchableOpacity} from 'react-native'
 
 import RNPickerSelect from 'react-native-picker-select'
 
-const RegisterScreen = ({ navigation }) => {
+const RegisterScreen = ({navigation}) => {
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [university, setUniversity] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [university, setUniversity] = useState('')
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const universities = [
+    {
+      label: 'University of Jos',
+      value: 'university of jos',
+    },
+    {
+      label: 'Havard University',
+      value: 'havard university',
+    },
+    {
+      label: 'Covenant University',
+      value: 'covenant university',
+    },
+  ]
 
-    const universities = [
-        {
-          label: "University of Jos",
-          value: "university of jos",
-        },
-        {
-          label: "Havard University",
-          value: "havard university",
-        },
-        {
-          label: "Covenant University",
-          value: "covenant university",
-        },
-      ];
+  const handleRegister = () => {
+    console.log('Email:', email)
+    console.log('Password:', password)
+  }
 
-    const handleRegister = () => {
-        
-        console.log('Email:', email);
-        console.log('Password:', password);
-    };
+  return (
+    <View style={styles.container}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>Sign Up</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        inputMode="text"
+        onChangeText={text => setFirstname(text)}
+        value={firstname}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Last Name"
+        inputMode="text"
+        onChangeText={text => setLastname(text)}
+        value={lastname}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Student Email"
+        inputMode="email"
+        onChangeText={text => setEmail(text)}
+        value={email}
+        autoCapitalize="none"
+      />
 
-    return (
-        <View style={styles.container}>
-                <View style={styles.headingContainer}>
-                    <Text style={styles.heading}>Sign Up</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    placeholder="First Name"
-                    inputMode='text'
-                    onChangeText={(text) => setFirstname(text)}
-                    value={firstname}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Last Name"
-                    inputMode='text'
-                    onChangeText={(text) => setLastname(text)}
-                    value={lastname}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Student Email"
-                    inputMode='email'
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    autoCapitalize='none'
-                />
+      <RNPickerSelect
+        style={pickerSelectStyles}
+        value={university}
+        onValueChange={value => {
+          setUniversity(value)
+        }}
+        items={universities}
+        placeholder={{label: 'University', value: ''}}
+      />
 
-                <RNPickerSelect
-                    style={pickerSelectStyles}
-                    value={university}
-                    onValueChange={(value) => {setUniversity(value)}}
-                    items={universities}
-                    placeholder={{ label: 'University', value: ''}}
-                />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={text => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    onChangeText={(text) => setPassword(text)}
-                    value={password}
-                    secureTextEntry
-                />
+      <TouchableOpacity style={{width: '100%'}} onPress={handleRegister}>
+        <View style={styles.registerButton}>
+          <Text style={styles.registerText}>Continue</Text>
+        </View>
+      </TouchableOpacity>
 
-                <TouchableOpacity style={{ width: '100%'}} onPress={handleRegister}>
-                    <View style={styles.registerButton}>
-                        <Text style={styles.registerText}>Continue</Text>
-                    </View>
-                </TouchableOpacity>
-
-                <View style={{ paddingVertical: 24}}>
-                    <Text style={{ color: '#2E3E5C'}}>
-                        Already have an account? <Text 
-                                style={{ color: '#6B4EFF'}} 
-                                onPress={() => navigation.navigate('Login')}>
-                                    Login
-                                </Text>
-                    </Text>
-                </View>
-            </View>
-    );
+      <View style={{paddingVertical: 24}}>
+        <Text style={{color: '#2E3E5C'}}>
+          Already have an account?{' '}
+          <Text
+            style={{color: '#6B4EFF'}}
+            onPress={() => navigation.navigate('Login')}
+          >
+            Login
+          </Text>
+        </Text>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingHorizontal: 23,
-        marginTop: 50,
-    },
-    input: {
-        width: '100%',
-        height: 48,
-        borderColor: '#E3E5E5',
-        borderWidth: 1,
-        borderRadius: 8,
-        marginBottom: 16,
-        paddingHorizontal: 8,
-    },
-    headingContainer: {
-        justifyContent: 'flex-start', 
-        width: '100%',
-        paddingVertical: 50
-    },
-    heading: {
-        fontWeight: 'bold',
-        fontSize: 32,
-        color: '#090A0A'
-    },
-    registerButton: {
-        width: '100%',
-        alignItems: 'center',
-        padding: 16,
-        backgroundColor: '#6B4EFF',
-        borderRadius: 48
-    },
-    registerText: {
-        color: 'white'
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 23,
+    marginTop: 50,
+  },
+  input: {
+    width: '100%',
+    height: 48,
+    borderColor: '#E3E5E5',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  headingContainer: {
+    justifyContent: 'flex-start',
+    width: '100%',
+    paddingVertical: 50,
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#090A0A',
+  },
+  registerButton: {
+    width: '100%',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#6B4EFF',
+    borderRadius: 48,
+  },
+  registerText: {
+    color: 'white',
+  },
 })
 
 const pickerSelectStyles = StyleSheet.create({
-    inputIOS: {
+  inputIOS: {
     //   fontSize: 16,
-      height: 48,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
-      borderWidth: 1,
-      borderColor: '#E3E5E5',
-      borderRadius: 8,
-      color: 'black',
-      marginBottom: 16,
-      paddingHorizontal: 8,
-      paddingRight: 30, // to ensure the text is not obscured by the arrow icon
-    },
-});
-  
+    height: 48,
+    paddingVertical: 12,
+    borderWidth: 1,
+    borderColor: '#E3E5E5',
+    borderRadius: 8,
+    color: 'black',
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    paddingRight: 30, // to ensure the text is not obscured by the arrow icon
+  },
+})
 
-export default RegisterScreen;
+export default RegisterScreen
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,200 +1,280 @@
-import React, { useState, useRef } from 'react';
+import {useState, useRef} from 'react'
 import {
-    View, 
-    Text, 
-    Image, 
-    StyleSheet, 
-    Dimensions, 
-    ScrollView, 
-    TouchableOpacity } from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native'
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+} from '@expo-google-fonts/inter'
 
-import { AntDesign } from '@expo/vector-icons'
+import {Feather} from '@expo/vector-icons'
 
-import purpleBlob from '../assets/purpleblob.png'
-import whiteBlob from '../assets/whiteblob.png'
+import colors from '../config/colors'
 
-const { width } = Dimensions.get('window');
+// import svg images
+import Onboarding1 from '../assets/onboarding1.svg'
+import Onboarding2 from '../assets/onboarding2.svg'
+import Onboarding3 from '../assets/onboarding3.svg'
+import PurpleBlob from '../assets/purpleblob.svg'
+import WhiteBlob from '../assets/whiteblob.svg'
 
-const WelcomeScreen = ({ navigation }) => {
+const {width} = Dimensions.get('window')
 
-    const [currentPage, setCurrentPage] = useState(0);
-    const scrollViewRef = useRef(null);
+const WelcomeScreen = ({navigation}) => {
+  console.log(Onboarding1, Onboarding2, Onboarding3)
+  const [currentPage, setCurrentPage] = useState(0)
+  const scrollViewRef = useRef(null)
 
-    const onboardingData = [
-        {
-          title: 'Welcome to buycorrect!',
-          description: 'The ultimate marketplace designed exclusively for students like you.',
-          image: require('../assets/onboarding1.png'),
-        },
-        {
-          title: 'Browse and Discover',
-          description: 'Explore our extensive marketplace categories and discover a treasure trove of student essentials.',
-          image: require('../assets/onboarding2.png'),
-        },
-        {
-          title: 'Get Started',
-          description: 'Ready to embark on your journey? Join thousands of students today and unlock a world of opportunities.',
-          image: require('../assets/onboarding3.png'),
-        },
-      ];
-    
-      const handleNext = () => {
-        if (currentPage < onboardingData.length - 1) {
-          scrollViewRef.current.scrollTo({ x: width * (currentPage + 1), animated: true });
-          setCurrentPage(currentPage + 1);
-        } else {
-          navigation.navigate('Login');
-        }
-      };
-    
-      const handleSkip = () => {
-        navigation.navigate('Login');
-      };
-    
-      const handleScroll = (event) => {
-        const { contentOffset } = event.nativeEvent;
-        const pageIndex = Math.round(contentOffset.x / width);
-        setCurrentPage(pageIndex);
-      };
+  let [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+  })
 
-    return (
-        <View style={styles.container}>
-            <ScrollView
-                ref={scrollViewRef}
-                horizontal
-                pagingEnabled
-                showsHorizontalScrollIndicator={false}
-                onScroll={handleScroll}
-                scrollEventThrottle={16}
+  if (!fontsLoaded) {
+    return null
+  }
+
+  const onboardingData = [
+    {
+      title: 'Welcome to buycorrect!',
+      description:
+        'The ultimate marketplace designed exclusively for students like you.',
+      image: Onboarding1,
+    },
+    {
+      title: 'Browse and Discover',
+      description:
+        'Explore our extensive marketplace categories and discover a treasure trove of student essentials.',
+      image: Onboarding2,
+    },
+    {
+      title: 'Get Started',
+      description:
+        'Ready to embark on your journey? Join thousands of students today and unlock a world of opportunities.',
+      image: Onboarding3,
+    },
+  ]
+
+  const handleNext = () => {
+    if (currentPage < onboardingData.length - 1) {
+      scrollViewRef.current.scrollTo({
+        x: width * (currentPage + 1),
+        animated: true,
+      })
+      setCurrentPage(currentPage + 1)
+    } else {
+      navigation.navigate('Login')
+    }
+  }
+
+  const handleSkip = () => {
+    navigation.navigate('Login')
+  }
+
+  const handleScroll = event => {
+    const {contentOffset} = event.nativeEvent
+    const pageIndex = Math.round(contentOffset.x / width)
+    setCurrentPage(pageIndex)
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={16}
+      >
+        {onboardingData.map(
+          ({title, description, image: OnboardingImage}, index) => (
+            <View
+              key={index}
+              style={[
+                styles.slide,
+                index % 2 !== 0 ? styles.secondarySlide : styles.primarySlide,
+              ]}
             >
-                {onboardingData.map((item, index) => (
-                    <View key={index} style={(index%2 !== 0 ? styles.oddSlide : styles.slide)}>
-                        <Image source={item.image} style={styles.image} />
-                        <Text style={[styles.title, { color: index%2 !== 0 ? 'black' : 'white'}]}>{item.title}</Text>
-                        <Text style={[styles.description, { color: index%2 !== 0 ? 'black' : 'white'}]}>{item.description}</Text>
-                        
-                        {/* skip button */}
-                        <TouchableOpacity style={styles.skipButton} onPress={handleSkip}>
-                            <Text style={(index%2 !== 0 ? styles.oddSkipButtonText : styles.skipButtonText)}>Skip</Text>
-                        </TouchableOpacity>
-                        
-                        {/* scroll indicator */}
-                        <View style={styles.indicatorContainer}>
-                            {onboardingData.map((_, index) => (
-                            <View
-                                key={index}
-                                style={[styles.indicator, index === currentPage ? styles.activeIndicator : null]}
-                            />
-                            ))}
-                        </View>
+              {/* onboarding content */}
+              <View style={styles.content}>
+                <OnboardingImage
+                  style={styles.image}
+                  width={300}
+                  height={300}
+                />
+                <Text
+                  style={[
+                    styles.title,
+                    {color: index % 2 !== 0 ? colors.inkDarkest : colors.white},
+                  ]}
+                >
+                  {title}
+                </Text>
+                <Text
+                  style={[
+                    styles.description,
+                    {color: index % 2 !== 0 ? colors.inkDarkest : colors.white},
+                  ]}
+                >
+                  {description}
+                </Text>
+              </View>
 
+              {/* skip button */}
+              <TouchableOpacity style={styles.skipButton} onPress={handleSkip}>
+                <Text
+                  style={[
+                    styles.skipButtonText,
+                    {color: index % 2 !== 0 ? colors.inkDarkest : colors.white},
+                  ]}
+                >
+                  Skip
+                </Text>
+              </TouchableOpacity>
 
-                        <View style={styles.blob}>
-                            <Image source={index%2 !== 0 ? purpleBlob : whiteBlob} />
-                        </View>
-
-                        {/* next button */}
-                        <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
-                        {/* style={styles.nextButtonText} color={`${index%2!==0 ? "white":'#583AEE'}`} */}
-                            <AntDesign name='arrowright' style={[styles.nextButtonText, { color: index % 2 !== 0 ? 'white' : '#583AEE', fontSize: 26 }]} />
-                            {/* <Text style={styles.nextButtonText}>{currentPage === onboardingData.length - 1 ? 'Login' : 'Next'}</Text> */}
-                        </TouchableOpacity>
-                        
-                    </View>
+              {/* scroll indicator */}
+              <View style={styles.indicatorContainer}>
+                {onboardingData.map((_, index) => (
+                  <View
+                    key={index}
+                    style={[
+                      styles.indicator,
+                      index === currentPage && index % 2 !== 0
+                        ? styles.activeIndicatorPrimary
+                        : index === currentPage && index % 2 === 0
+                        ? styles.activeIndicatorSecondary
+                        : null,
+                    ]}
+                  />
                 ))}
+              </View>
 
+              <View style={styles.blobContainer}>
+                {index % 2 !== 0 ? (
+                  <PurpleBlob style={styles.blob} />
+                ) : (
+                  <WhiteBlob style={styles.blob} />
+                )}
+              </View>
 
-            </ScrollView>
-        </View>
-    );
+              <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
+                <Feather
+                  name="arrow-right"
+                  size={30}
+                  style={{
+                    color: index % 2 !== 0 ? colors.white : colors.inkDarkest,
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+        )}
+      </ScrollView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-      },
-      oddSlide: {
-        width,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        backgroundColor: '#fff',
-      },
-      slide: {
-        width,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        backgroundColor: '#583AEE',
-      },
-      image: {
-        width: 350,
-        height: 322,
-        marginBottom: 30,
-      },
-      title: {
-        fontSize: 24,
-        fontWeight: 'medium',
-        marginBottom: 10,
-      },
-      description: {
-        fontSize: 14,
-        textAlign: 'center',
-        color: 'white'
-      },
-      buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        marginTop: 20,
-      },
-      skipButton: {
-        padding: 10,
-        position: 'absolute',
-        top: 60,
-        right: 20
-      },
-      oddSkipButtonText: {
-        fontSize: 16,
-        color: 'black',
-      },
-      skipButtonText: {
-        fontSize: 16,
-        color: '#fff',
-      },
-      nextButton: {
-        position: 'absolute',
-        bottom: 90,
-        right: 50
-      },
-      nextButtonText: {
-        fontSize: 16,
-        color: '#fff',
-      },
-      indicatorContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        bottom: 90,
-        left: 20,
-      },
-      indicator: {
-        width: 8,
-        height: 8,
-        borderRadius: 4,
-        backgroundColor: '#888',
-        marginHorizontal: 4,
-      },
-      activeIndicator: {
-        backgroundColor: '#fff',
-      },
-      blob: {
-        position: 'absolute',
-        right: 0,
-        bottom: 0,
-        zIndex: -2
-      },
+  container: {
+    flex: 1,
+  },
+
+  slide: {
+    width,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  secondarySlide: {
+    backgroundColor: colors.white,
+  },
+
+  primarySlide: {
+    backgroundColor: colors.primaryDarkest,
+  },
+
+  content: {
+    alignItems: 'center',
+    marginTop: 150,
+  },
+
+  image: {
+    marginBottom: 50,
+  },
+
+  title: {
+    fontFamily: 'Inter_500Medium',
+    fontSize: 24,
+    fontWeight: 'medium',
+    marginBottom: 20,
+  },
+  description: {
+    fontFamily: 'Inter_400Regular',
+    fontSize: 14,
+    textAlign: 'center',
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  skipButton: {
+    padding: 10,
+    position: 'absolute',
+    top: 60,
+    right: 20,
+  },
+  skipButtonText: {
+    fontSize: 16,
+    color: colors.white,
+  },
+  nextButton: {
+    position: 'absolute',
+    bottom: 65,
+    right: 30,
+    padding: 10,
+  },
+
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 90,
+    left: 30,
+  },
+  indicator: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: colors.skyDark,
+    marginHorizontal: 4,
+  },
+  activeIndicatorPrimary: {
+    backgroundColor: colors.primaryDarkest,
+  },
+  activeIndicatorSecondary: {
+    backgroundColor: colors.white,
+  },
+  blobContainer: {
+    width: 134,
+    height: 206,
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  blob: {
+    right: -10,
+  },
 })
 
-export default WelcomeScreen;
+export default WelcomeScreen
